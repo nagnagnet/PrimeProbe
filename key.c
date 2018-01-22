f30a612a56c0f1f9e6e346b2b0629a498b07a987
nagnagnet <nag@nag.net>
2018-01-23 02:39:29 +0900

    draft

---
+++ key.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#define N 3072
+#define RDTSC(X) asm volatile("lfence;rdtsc; shlq $32, %%rdx; orq %%rdx, %%rax":"=r"(X) :: "%rdx")
+
+struct st{
+	int num;
+	char name[60];
+};
+
+int main(){
+
+	int i, j, k, n;
+	struct st *buf;
+	buf = (struct st*)malloc(sizeof(struct st)*N);
+	for(i = 0; i < N; i++) buf[i].num = i;
+
+	printf("%p\n",&buf[0]);	
+
+	while(1){
+		for(i = 0; i < N; i++){
+			for(j = 0; j < 250000*10; j++) n = buf[0].num;
+			if(i % 2 == 0){
+				for(j = 0; j < N; j++){
+					for(k = 1; k < 20*250000/N; k++) n = buf[j].num;
+				}
+			} else {
+				for(j = 0; j < N; j++){
+					for(k = 1; k < 2*20*250000/N; k++) n = buf[j].num;
+				}
+			}
+		}
+	}
+	return 0;
+}
+
